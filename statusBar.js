c9f5eff04eb3ecbf74ce7f4b23d6a6b82732bcc4
Martin Adamko <martin@attitude.sk>
2014-08-20 02:29:46 +0200

    Update statusBar.js
    Since only iOS uses translucent statusbar, it's possible to test against iOS version 4-8 to date. This version has 20px status bar height baked in, because there's no reason to calculate it on-the-fly – there's either no pushing down or there's 20px push down.
    
    Usage: Use html conditional class to add padding to any element required.
    
    Note: iOS 4 requires classList API polyfill.

--- statusBar.js
+++ statusBar.js
@@ -1,56 +1,141 @@
-/*! statusBar.js v0.1.0 | (c) 2014 Martin Adamko | License: MIT */
+/*! statusBar.js v0.2.0 | (c) 2014 Martin Adamko | Licence MIT */
 /*jslint browser: true*/
-(function(window, screen, document, body) {
-    'use strict';
-    var webViewHead,
-        webViewLayers,
-        webViewLayersLength,
-        statusHeight,
-        statusHeightDifference,
-        debug = window.location.search.match(/[\?&]debug-standalone=true/) ? true : false,
-        topOffset = function() {
-            var i;
+(function(window) {
+    var debug = window.location.search.match(/[\?&]debug-standalone=true/),
+        isiOS = !!window.navigator.userAgent.match(/(iPad|iPhone|iPod)/),
+        iOSVersion = null,
+        // The Status Bar object
+        StatusBar = {
+        	running: false,
+            active: null,
+            offset: 0,
+            // Cache screen size since on iOS device has fixed screen size
+            screen: {
+                height: window.screen.height,
+                width:  window.screen.width,
+                orientation: {}
+            },
+            init: function() {
+                if (debug) window.console.log('Initialize');
 
-            // Fix iOS scrolling out of view when landscape
-            body.scrollTop = 0;
+                StatusBar.running = true;
 
-            // Portrait mode
-            if (window.innerHeight > window.innerWidth) {
-                statusHeight = window.innerHeight - screen.availHeight;
-            } else {
-                statusHeight = window.innerWidth - screen.availHeight;
+                // Apply html.standalone class
+                if (StatusBar.prototype.htmlClasses.standalone) {
+                    window.document.body.parentElement.classList.add(StatusBar.prototype.htmlClasses.standalone);
+                }
+
+                // Attach event listener to listen for a change event trigger
+                window.addEventListener(StatusBar.prototype.changeEventName, StatusBar.update, false);
+
+                // Run first time
+                StatusBar.update();
+            },
+            update: function() {
+                // Set screen orientation
+                StatusBar.screen.orientation = window.innerHeight > window.innerWidth ? {
+                    display: 'portrait',
+                    vertical: true,
+                    horizontal: false
+                } : {
+                    display: 'landscape',
+                    vertical: false,
+                    horizontal: true
+                };
+
+                // In vertical orientation
+                if (StatusBar.screen.orientation.vertical) {
+                    // Is not active, when window fills the screen
+                    StatusBar.active = !(
+                        window.innerHeight === StatusBar.screen.height &&
+                        window.innerWidth  === StatusBar.screen.width
+                    );
+
+                    // Offset can be only 20px or 0 when the status bar is active
+                    StatusBar.offset = StatusBar.active ? 0 : 20;
+                } else {
+                    // Is not active, when window fills the screen, but
+                    // in horizontal position, window.innerHeight fits screen.width
+                    StatusBar.active = !(
+                        window.innerHeight === StatusBar.screen.width &&
+                        window.innerWidth  === StatusBar.screen.height
+                    );
+
+                    // Offset can be only 20px; Since iOS 8 however the landspape mode
+                    // runs as fullscreen without the status bar visible
+                    StatusBar.offset = iOSVersion >= 8 ? 0 : 20;
+                }
+
+                if (debug) window.console.log('StatusBar active:', StatusBar.active);
+
+                // Toggle the html class based on the calculated offset
+                StatusBar.prototype.toggleClass(StatusBar.offset);
+
+                // Trigger the Status Bar Changed event
+                StatusBar.prototype.triggerEvent();
+            },
+            prototype: {
+                changeEventName: 'resize',
+                triggerEventName: 'statusbarchange',
+                htmlClasses: {
+                    statusbar: 'standalone-statusbar-active',
+                    standalone: 'standalone'
+                },
+                ignoreVersion: false,
+                triggerEvent: function() {
+                    var e = window.document.createEvent('Event');
+
+                    e.initEvent(this.triggerEventName, true, false);
+                    window.dispatchEvent(e);
+                },
+                toggleClass: function(size) {
+                    if (!this.htmlClasses.statusbar) {
+                        return;
+                    }
+
+                    if (!size) {
+                        window.document.body.parentElement.classList.remove(this.htmlClasses.statusbar);
+
+                        return;
+                    }
+
+                    window.document.body.parentElement.classList.add(this.htmlClasses.statusbar);
+                }
             }
-
-            if (statusHeight <= 0) {
-                statusHeight = 0;
-            }
-
-            // webViewHead.style.marginTop  = -statusHeightDifference + 'px';
-            webViewHead.style.paddingTop = statusHeight + 'px';
-            for (i = 0; i < webViewLayersLength; i += 1) {
-                webViewLayers[i].style.paddingTop = statusHeight + 'px';
-            }
-        },
-        init = function() {
-            // Apply standalone class the HTML tag
-            body.parentElement.classList.add('standalone');
-            // Fix iOS scrolling out of view when landscape
-            body.scrollTop = 0;
-
-            // View header element
-            webViewHead = document.getElementById('view');
-            // View layer elements like modals etc.
-            webViewLayers = document.getElementsByClassName('view-layer');
-            // Cache length
-            webViewLayersLength = webViewLayers.length;
         };
 
-    if (debug || (typeof window.navigator.standalone === 'boolean' && window.navigator.standalone)) {
-        init();
+    // Run only in standalone mode
+    if (debug || (typeof window.navigator.standalone === 'boolean' && window.navigator.standalone === true)) {
+        // This code is required only for iOS
+        if (isiOS) {
+            // Sniff the version from the User Agent string
+            iOSVersion = window.navigator.userAgent.match(/\w+\s+OS\s+(\d)/);
+            iOSVersion = iOSVersion ? parseInt(iOSVersion[1], 10) : 0;
+            if (debug) window.console.log('iOS v', iOSVersion);
 
-        // Calculate & apply top margin when in standalone
-        window.addEventListener('mediaexperiencechanged', function(e) {
-            topOffset();
-        });
+            // Was not able to test version of iOS < 4.3.2
+            if (iOSVersion >= 4 || StatusBar.prototype.ignoreVersion) {
+                // Activating on document ready should leave room to modify any settings
+                window.document.onreadystatechange = function() {
+                    if (window.document.readyState == "complete") {
+                        if (debug) window.console.info('StatusBar: Document ready initialise...');
+
+                        // Initialize for the first time
+                        StatusBar.init();
+                    }
+                };
+
+				// Fallback for iOS 4
+                if (!StatusBar.running) {
+                	if (debug) window.console.info('StatusBar: Window load initialise...');
+
+                    // Initialize for the first time
+                    window.addEventListener('load', StatusBar.init, false);
+				}
+            }
+
+            // Export as global object
+            window.StatusBar = StatusBar;
+        }
     }
-}(window, window.screen, document, window.document.body));
+}(window));
